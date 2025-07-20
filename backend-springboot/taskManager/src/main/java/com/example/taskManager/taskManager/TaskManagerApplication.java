00a4c3413c6a4790bba7c8795ffbc3f3ca785b3a
joebin02 <imstevejoebz@gmail.com>
2025-07-20 23:31:25 +0530

    new changes 2007

--- backend-springboot/taskManager/src/main/java/com/example/taskManager/taskManager/TaskManagerApplication.java
+++ backend-springboot/taskManager/src/main/java/com/example/taskManager/taskManager/TaskManagerApplication.java
@@ -4,9 +4,11 @@ import com.example.taskManager.taskManager.repository.TaskRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.taskManager.taskManager.entity")
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
